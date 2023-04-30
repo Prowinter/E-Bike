980d2781ca4aef0f6b23c33b903f50b876c77aba
prowinter <prowinter@aliyun.com>
2023-04-30 10:19:13 +0800

    feat:add odrive can drive

--- src/bicycle/src/balance.cpp
+++ src/bicycle/src/balance.cpp
@@ -4,22 +4,38 @@
 #include <ros/console.h>
 #include <angles/angles.h>
 #include "sensor_msgs/Imu.h"
-#include <can_communication/CAN.h>
-#include <can_communication/VESC.h>
-#include <can_communication/Bicycle.h>
+#include <can_communication/CAN_srv.h>
+#include <can_communication/VESC_srv.h>
+#include <can_communication/Odrive_srv.h>
+#include <can_communication/Odrive_msg.h>
+#include <can_communication/Bicycle_msg.h>
 #include <dynamic_reconfigure/server.h>
 #include <bicycle/LQRConfig.h>
 #include <std_msgs/Float32.h>
+#include <ros_odrive/odrive_ctrl.h>
+#include <bicycle/Bicycle_Roll.h>
 
 #define Time_Sec 0.006f
-// #define _DEBUG_IMU 1
-#define _DEBUG_Dynamic_LQR 1
-// #define _DEBUG_VESC_Motor_Speed 1
 
+// #define _DEBUG_IMU 1
+// #define _DEBUG_Dynamic_LQR 1
+
+// #define USE_USB_Motor_Control 1
 
 #define CAN_ID 0x0E
 #define VESC_ID_1 0x68
 #define VESC_ID_2 0x69
+
+#define Angle_Offset 1.5f
+
+enum commands {
+    CMD_AXIS_RESET,
+    CMD_AXIS_IDLE,
+    CMD_AXIS_CLOSED_LOOP,
+    CMD_AXIS_SET_VELOCITY,
+    CMD_AXIS_SET_VELOCITY_DUAL,
+    CMD_REBOOT
+};
 
 struct BikeStructure{             // Structure declaration
   double LQR_Kp=1806.1;
@@ -27,119 +43,63 @@ struct BikeStructure{             // Structure declaration
   double LQR_Ks=0.0102;
   int Servo_Direction_Angle;
   float Drive_Motor_duty;
-  int   Balance_Motor_rpm;
+  float Balance_Motor_rpm;
   float Bicycle_voltage;
   double Roll_Angular_Velocity = 0.0f;
   double Roll;
   double Roll_Gyro;
+  bool Switch;
 } Ebike;     // Structure variable
-
-double data[5] = {0};    // 存储数据的数组
-int sum = 0;    // 记录数据总和的变量
-int i;    // 循环计数器
 
 
 ros::ServiceClient CAN_Servo_Client;
 ros::ServiceClient VESC_Motor_1_Client;
 ros::ServiceClient VESC_Motor_2_Client;
+ros::ServiceClient Odrive_Motor_Client;
 ros::Publisher IMU_ROLL_Pbu;
+ros::Publisher Odrive_pub;
+ros::Publisher Roll_pub;
+ros::Publisher Roll_Velocity_pub;
 ros::Timer timer;
-
-bool Switch = false;
 
 void callback(bicycle::LQRConfig &config, uint32_t level) {
   Ebike.LQR_Kp = config.LQR_Kp;
   Ebike.LQR_Kv = config.LQR_Kv;
   Ebike.LQR_Ks = config.LQR_Ks;
-  Switch = config.LQR_Switch;
- //ROS_WARN("Reconfigure Request: %f %f %f %d", Ebike.LQR_Kp, Ebike.LQR_Kv, Ebike.LQR_Ks, Switch);
-  // #ifdef _DEBUG_IMU
-  // ROS_INFO("Reconfigure Request: %f %f %f", Ebike.LQR_Kp, Ebike.LQR_Kv, Ebike.LQR_Ks);
-  // #endif
+  Ebike.Switch = config.LQR_Switch;
 }
 
 void timerCallback(const ros::TimerEvent& event)
 {
-  // static double last_ang;
-  // double Temp_Roll_Gyro ;
-
-  // sum += Ebike.Roll_Gyro;
-  // sum -= data[0];
-  // for (i = 0; i < 5 - 1; i++)
-  //   {
-  //     data[i] = data[i+1];
-  //   }
-  //     data[5-1] = Ebike.Roll_Gyro;
-  //     Temp_Roll_Gyro = sum/5.0;
-    
-  // //Ebike.Roll_Angular_Velocity = (Ebike.Roll-last_ang)/Time_Sec;
-  // // int Motor_Set_Rpm =(int)(Ebike.Roll * Ebike.LQR_Kp + Ebike.Roll_Angular_Velocity * Ebike.LQR_Kv + Ebike.Balance_Motor_rpm * Ebike.LQR_Ks*6.28f);
-  // int Motor_Set_Rpm =(int)(Ebike.Roll * Ebike.LQR_Kp + Temp_Roll_Gyro * Ebike.LQR_Kv + Ebike.Balance_Motor_rpm * Ebike.LQR_Ks);
-  // // ROS_WARN("Roll_Gyro:%f\t Roll:%f", Ebike.Roll_Gyro,Ebike.Roll);
-  // //ROS_WARN("Reconfigure Request: %f %f %f %d", Ebike.LQR_Kp, Ebike.LQR_Kv, Ebike.LQR_Ks, Switch);
-  // Motor_Set_Rpm = (Motor_Set_Rpm < 20000) ? (Motor_Set_Rpm > -20000) ? Motor_Set_Rpm : -20000 : 20000;
-  // //ROS_WARN("Roll_Angular_Velocity:%f,Motor_Set_Rpm=%d,Ebike.Roll=%f,Balance_Motor_rpm=%d",Ebike.Roll_Angular_Velocity,Motor_Set_Rpm,Ebike.Roll,Ebike.Balance_Motor_rpm );
-
-  // last_ang = Ebike.Roll;
-  // if(Switch){
-  //   can_communication::VESC srv;
-  //   srv.request.Motor_rpm = - Motor_Set_Rpm;
-  //   if(!VESC_Motor_1_Client.call(srv))
-  //   {
-  //     ROS_ERROR("Failed to call service VESC_Motor_1_Client");
-  //   }
-  // }
 }
 
-void CAN_Callback(const can_communication::Bicycle& msg){
+void CAN_Callback(const can_communication::Bicycle_msg& msg){
   if(msg.Device_ID == CAN_ID){
     Ebike.Servo_Direction_Angle = msg.Servo_Direction;
   }
 }
 
-void Motor_1_Callback(const can_communication::Bicycle& msg){
+void Motor_1_Callback(const can_communication::Bicycle_msg& msg){
   static int flag = 0;
   double Temp_Roll_Gyro ;
   if(msg.Device_ID == VESC_ID_1){
     Ebike.Balance_Motor_rpm = msg.Motor_rpm;
     Ebike.Bicycle_voltage = msg.Bicycle_voltage;
-    Ebike.Roll = msg.Motor_Roll;
-    Ebike.Roll_Gyro = msg.Motor_Roll_Gyro;
-    // ROS_INFO("Roll:%f", Ebike.Roll - Angle_Offset);
-
-    sum += Ebike.Roll_Gyro;
-    sum -= data[0];
-    for (i = 0; i < 5 - 1; i++)
-      {
-        data[i] = data[i+1];
-      }
-        data[5-1] = Ebike.Roll_Gyro;
-        Temp_Roll_Gyro = sum/5.0;
-      
-    int Motor_Set_Rpm =(int)(Ebike.Roll * Ebike.LQR_Kp + Temp_Roll_Gyro * Ebike.LQR_Kv + Ebike.Balance_Motor_rpm * Ebike.LQR_Ks);
-    Motor_Set_Rpm = (Motor_Set_Rpm < 20000) ? (Motor_Set_Rpm > -20000) ? Motor_Set_Rpm : -20000 : 20000;
-
-    if(Switch){
-      can_communication::VESC srv;
-      srv.request.Motor_rpm = - Motor_Set_Rpm;
-      if(!VESC_Motor_1_Client.call(srv))
-      {
-        ROS_ERROR("Failed to call service VESC_Motor_1_Client");
-      }
-    }
-    // if(!flag) {flag = 1;timer.start();}
   }
 }
 
-void Motor_2_Callback(const can_communication::Bicycle& msg){
-  
+void Motor_2_Callback(const can_communication::Bicycle_msg& msg){
   if(msg.Device_ID == VESC_ID_2){
     Ebike.Drive_Motor_duty = msg.Motor_duty;
   }
 }
 
-void imuCallback(const sensor_msgs::ImuConstPtr& msg){
+void Odrive_Motor_Callback(const can_communication::Odrive_msg& msg){
+  Ebike.Balance_Motor_rpm = msg.Motor_rpm;
+  Ebike.Bicycle_voltage   = msg.Motor_voltage;
+}
 
+void imuCallback(const sensor_msgs::ImuConstPtr& msg){
   
   tf2::Quaternion q(
       msg->orientation.x,
@@ -148,37 +108,50 @@ void imuCallback(const sensor_msgs::ImuConstPtr& msg){
       msg->orientation.w);
   tf2::Matrix3x3 m(q);
   double roll, pitch, yaw;
-  m.getRPY(pitch, roll, yaw);  //change direction
-  ROS_WARN("roll:%.3f ,pitch:%.3f ,yaw:%.3f",angles::to_degrees(roll),angles::to_degrees(pitch),angles::to_degrees(yaw));
+  m.getRPY(roll, pitch, yaw);  //change direction
+  Ebike.Roll = - angles::to_degrees(roll)+Angle_Offset;
+  Ebike.Roll_Angular_Velocity = - msg->angular_velocity.x;
+
+  bicycle::Bicycle_Roll roll_msg;
+  roll_msg.Roll = Ebike.Roll;
+  roll_msg.Roll_Velocitys = Ebike.Roll_Angular_Velocity;
+  Roll_pub.publish(roll_msg);
+
   #ifdef _DEBUG_IMU
-  // ROS_INFO("roll:%.3f ,pitch:%.3f ,yaw:%.3f",angles::to_degrees(roll)-Angle_Offset,angles::to_degrees(pitch),angles::to_degrees(yaw));
-  // ROS_INFO("Balance_Motor_rpm:%d, Drive_Motor_duty:%.3f, Bicycle_voltage:%.3f, Servo_Direction_Angle:%d",  \
-  //           Ebike.Balance_Motor_rpm, \
-  //           Ebike.Drive_Motor_duty,  \
-  //           Ebike.Bicycle_voltage,   \
-  //           Ebike.Servo_Direction_Angle);
+  ROS_WARN("roll:%.3f ,pitch:%.3f ,yaw:%.3f",angles::to_degrees(roll),angles::to_degrees(pitch)-Angle_Offset,angles::to_degrees(yaw));
   #endif
 
-  // std_msgs::Float32 roll_msg;
-  // roll_msg.data = angles::to_degrees(roll);
-  // Ebike.Roll = angles::to_degrees(roll);
-  // IMU_ROLL_Pbu.publish(roll_msg);
-  
-  // int Motor_Set_Rpm =(int)(angles::to_degrees(roll) * Ebike.LQR_Kp + Ebike.Roll_Angular_Velocity * Ebike.LQR_Kv + Ebike.Balance_Motor_rpm * Ebike.LQR_Ks);
-  // // ROS_INFO("p:%.3f ,v:%.3f\r\n",Ebike.LQR_Kp,Ebike.LQR_Kv);
-  
-  // // #ifdef _DEBUG_IMU
-  // // ROS_INFO("Motor_Speed:%.3f",VESC_Motor_1_Client.);
-  // // #endif
-  // //ROS_INFO("rpm: %d\r\n", Motor_Set_Rpm);
+  int Motor_Set_Rpm =(int)(Ebike.Roll * Ebike.LQR_Kp + Ebike.Roll_Angular_Velocity * Ebike.LQR_Kv + Ebike.Balance_Motor_rpm * Ebike.LQR_Ks);
+  Motor_Set_Rpm = (Motor_Set_Rpm < 20) ? (Motor_Set_Rpm > -20) ? Motor_Set_Rpm : -20 : 20;
 
-  // can_communication::VESC srv;
-  // srv.request.Motor_rpm = Motor_Set_Rpm;
-  // if(!VESC_Motor_1_Client.call(srv))
-  // {
-  //   ROS_ERROR("Failed to call service VESC_Motor_1_Client");
-  // }
+  #ifdef USE_USB_Motor_Control
+  if(Ebike.Switch){
+    ros_odrive::odrive_ctrl msgg;
+    msgg.command = CMD_AXIS_SET_VELOCITY;
+    msgg.axis = 0;
+    msgg.fval = - Motor_Set_Rpm;
+    Odrive_pub.publish(msgg);
+  }else
+  {
+    ros_odrive::odrive_ctrl msgg;
+    msgg.command = CMD_AXIS_SET_VELOCITY;
+    msgg.axis = 0;
+    msgg.fval = 0;
+    Odrive_pub.publish(msgg);
 
+  }
+  #else
+  if(Ebike.Switch){
+    can_communication::Odrive_srv srv;
+    srv.request.Motor_vel = Motor_Set_Rpm;
+    if(!Odrive_Motor_Client.call(srv)) ROS_ERROR("Failed to call service Odrive_Motor_Client");
+  }else
+  {
+    can_communication::Odrive_srv srv;
+    srv.request.Motor_vel = 0;
+    if(!Odrive_Motor_Client.call(srv)) ROS_ERROR("Failed to call service Odrive_Motor_Client");
+  }
+  #endif
 }
 
 int main(int argc, char** argv){
@@ -186,30 +159,47 @@ int main(int argc, char** argv){
 
   // ros::NodeHandle nn;
   // IMU_ROLL_Pbu = nn.advertise<std_msgs::Float32>("/Bicycle/balance/imu_roll", 30);
+  ros::NodeHandle roll_node;
+  Roll_pub = roll_node.advertise<bicycle::Bicycle_Roll>("/bicycle/fdi/imu", 100);
+
 
   ros::NodeHandle node;
   ros::Subscriber sub = node.subscribe("fdi_imu", 100, &imuCallback);
 
+  // subscribe msg
   ros::NodeHandle can_node;
   ros::Subscriber can_sub = can_node.subscribe("Bicycle/CAN_Servo_info", 1000, &CAN_Callback);
   ros::NodeHandle motor_1_node;
   ros::Subscriber motor_1sub = motor_1_node.subscribe("Bicycle/VESC_Motor_1_info", 1000, &Motor_1_Callback);
   ros::NodeHandle motor_2_node;
   ros::Subscriber motor_2sub = motor_2_node.subscribe("Bicycle/VESC_Motor_2_info", 1000, &Motor_2_Callback);
+  ros::NodeHandle odrive_node;
+  ros::Subscriber odrive_sub = odrive_node.subscribe("Bicycle/Odrive_Motor_info", 1000, &Odrive_Motor_Callback);
 
-  // ros::NodeHandle service_a;
-  // CAN_Servo_Client = service_a.serviceClient<can_communication::CAN>("CAN_Servo");
-  // ros::NodeHandle service_b;
-  // VESC_Motor_1_Client = service_b.serviceClient<can_communication::VESC>("VESC_Motor_1");
+  // subscribe srv
+  ros::NodeHandle service_a;
+  CAN_Servo_Client = service_a.serviceClient<can_communication::CAN_srv>("CAN_Servo");
+  ros::NodeHandle service_b;
+  VESC_Motor_1_Client = service_b.serviceClient<can_communication::VESC_srv>("VESC_Motor_1");
   // ros::NodeHandle service_c;
-  // VESC_Motor_2_Client = service_c.serviceClient<can_communication::VESC>("VESC_Motor_2");
-  
+  // VESC_Motor_2_Client = service_c.serviceClient<can_communication::VESC_srv>("VESC_Motor_2");
+  #ifdef USE_USB_Motor_Control
+  ros::NodeHandle odrive_node;
+  Odrive_pub = odrive_node.advertise<ros_odrive::odrive_ctrl>("/ros_odrive/odrive_ctrl_355E30753533", 100);
+  #else
+  ros::NodeHandle service_d;
+  Odrive_Motor_Client = service_d.serviceClient<can_communication::Odrive_srv>("Odrive_Motor");
+  while(!ros::service::exists("Odrive_Motor", false));
+  #endif
+
+
   dynamic_reconfigure::Server<bicycle::LQRConfig> server;
   dynamic_reconfigure::Server<bicycle::LQRConfig>::CallbackType f;
   f = boost::bind(&callback, _1, _2);
   server.setCallback(f);
 
-  ros::NodeHandle nh;
+
+  // ros::NodeHandle nh;
   // timer = nh.createTimer(ros::Duration(Time_Sec), timerCallback);
   // timer.stop();
 
--- src/can_communication/scripts/Service.py
+++ src/can_communication/scripts/Service.py
@@ -9,6 +9,7 @@ import can
 import time
 import atexit
 
+from Odrive import *
 from CAN_Servo import *
 from VESC_Motor import *
 
@@ -18,15 +19,22 @@ def on_exit():
     notifier.stop()
     bus.shutdown()
 
+def timer_callback(event):
+    odrive_motor.Get_Pos_Vel()
+    odrive_motor.Get_Bus_Cur()
+    
+
 def Process_Message(msg):
     # rospy.logdebug("Received message:", msg.data)
     # 12 bits ADC -> Angle
-    if (msg.arbitration_id == 0x0E):
-        servo.Process_Message(msg)
-    elif  ((msg.arbitration_id & 0xFF) == motor_1.ID):
-        motor_1.Process_Message(msg)
-    elif  ((msg.arbitration_id & 0xFF) == motor_2.ID):
-        motor_2.Process_Message(msg)
+    # if (msg.arbitration_id == 0x0E):
+    #     servo.Process_Message(msg)
+    # elif  ((msg.arbitration_id & 0xFF) == motor_1.ID):
+    #     motor_1.Process_Message(msg)
+    # elif  ((msg.arbitration_id & 0xFF) == motor_2.ID):
+    #     motor_2.Process_Message(msg)
+    if((msg.arbitration_id >> 5 & 0xFF) == Odrive_Node_ID):
+        odrive_motor.Process_Message(msg)
     else:
         print("error")
 
@@ -54,11 +62,20 @@ def handle_motor_2(req):
     response.message = "OK"
     return response
 
+def handle_odrive_motor(req):
+    odrive_motor.Set_RPM(req.Motor_vel)
+    print("RPM={}".format(req.Motor_vel))
+    response = Odrive_srvResponse()
+    response.success = True
+    response.message = "OK"
+    return response
+
 rospy.init_node('can_server')
 
-can_servo_pub = rospy.Publisher('Bicycle/CAN_Servo_info', Bicycle, queue_size=10)
-motor_1_pub = rospy.Publisher('Bicycle/VESC_Motor_1_info', Bicycle, queue_size=10)
-motor_2_pub = rospy.Publisher('Bicycle/VESC_Motor_2_info', Bicycle, queue_size=10)
+# can_servo_pub = rospy.Publisher('Bicycle/CAN_Servo_info', Bicycle, queue_size=10)
+# motor_1_pub = rospy.Publisher('Bicycle/VESC_Motor_1_info', Bicycle, queue_size=10)
+# motor_2_pub = rospy.Publisher('Bicycle/VESC_Motor_2_info', Bicycle, queue_size=10)
+odrive_motor_pub = rospy.Publisher('Bicycle/Odrive_Motor_info', Odrive_msg, queue_size=100)
 
 logger = can.CSVWriter("/home/prowinter/Desktop/Bicycle_ws/log/log.csv") 
 
@@ -67,13 +84,16 @@ filters = [
     {"can_id": VESC_ID_1 | CAN_PACKET_STATUS_1 << 8, "can_mask": 0x1FFFFFFF, "extended": True},     # VESC-STATUS_1
     {"can_id": VESC_ID_2 | CAN_PACKET_STATUS_1 << 8, "can_mask": 0x1FFFFFFF, "extended": True},     # VESC-STATUS_1
     {"can_id": VESC_ID_1 | CAN_PACKET_STATUS_5 << 8, "can_mask": 0x1FFFFFFF, "extended": True},     # VESC-STATUS_5
+    {"can_id": Odrive_Node_ID << 5 | Axis0_Get_Encoder_Estimates, "can_mask": 0x7FF, "extended": False}, 
+    {"can_id": Odrive_Node_ID << 5 | Axis0_Get_Bus_Voltage_Current, "can_mask": 0x7FF, "extended": False}, 
 ]
 
-bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=1000000, can_filters=filters)
+bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000, can_filters=filters)
 
-servo = CAN_Servo(CAN_ID,bus,can_servo_pub)
-motor_1 = VESC_Motor(VESC_ID_1,bus,motor_1_pub)
-motor_2 = VESC_Motor(VESC_ID_2,bus,motor_2_pub)
+# servo = CAN_Servo(CAN_ID,bus,can_servo_pub)
+# motor_1 = VESC_Motor(VESC_ID_1,bus,motor_1_pub)
+# motor_2 = VESC_Motor(VESC_ID_2,bus,motor_2_pub)
+odrive_motor = Odrive_Motor(Odrive_Node_ID,bus,odrive_motor_pub)
 
 listeners = [
     Process_Message,    # Callback function, print the received messages
@@ -84,8 +104,10 @@ notifier = can.Notifier(bus, listeners)
 atexit.register(on_exit)
 
 
-a = rospy.Service('CAN_Servo', CAN, handle_can_servo)
-b = rospy.Service('VESC_Motor_1', VESC, handle_motor_1)
-c = rospy.Service('VESC_Motor_2', VESC, handle_motor_2)
-# motor_1.SetRPM(1000)
+
+# a = rospy.Service('CAN_Servo', CAN, handle_can_servo)
+# b = rospy.Service('VESC_Motor_1', VESC, handle_motor_1)
+# c = rospy.Service('VESC_Motor_2', VESC, handle_motor_2)
+d = rospy.Service('Odrive_Motor', Odrive_srv, handle_odrive_motor)
+timer = rospy.Timer(rospy.Duration(1.0/100.0), timer_callback)
 rospy.spin()
